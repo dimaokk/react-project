161db1472171bf0ac511fadaa70856fc559d95a0
dimaokk <dimadark216@gmail.com>
2022-02-17 17:07:24 +0700

    web

--- src/comp/common/Preloader/Preloader.jsx
+++ src/comp/common/Preloader/Preloader.jsx
@@ -3,8 +3,9 @@ import styles from "./Preloader.module.css";
 import load from "../../../assets/loading-buffering.gif";
 export const Preloader = () => {
   return (
+    // eslint-disable-next-line
     <div className={styles.cont}>
-      <img src={load} />
+      <img src={load} alt="sa" />
     </div>
   );
 };
